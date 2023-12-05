3608f611d6a30b4c2600ca0e36652fc0eac93576
Cucik <dukat91@gmail.com>
2023-12-05 22:02:02 +0100

    2nd commit

--- 1/1.py
+++ 1/1.py
@@ -8,18 +8,48 @@ final_results = []
 lineread = []
 pattern = r'\d'
 counter = 0
+
+word_to_number = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+}
+
+number_words = [
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9','wordscannotdescribemyconfusion'
+]
+final_results=0
+
 with open(file_path, 'r') as file:
     for line in file:
-#        print(line.strip())  # strip() removes any leading/trailing whitespace including newline characters
-        for character in line.strip():
-            if results[0] is None:
-                if re.match(pattern, character):
-                    results[0]=character
-            if re.match(pattern, character):
-                results[1]=character
-            print(results[0],results[1])
-        final_results[counter]=int(results[0])+int(results[1])
-        counter=counter+1
+        converted_table = []
+        match = re.search(pattern,line)
+        #print(match.group())
+        number_word_pattern = r'.*' + '|'.join(number_words) + r'.*'
+        found_number_words = re.findall(number_word_pattern, line)
+
+        print(found_number_words)
+
+        if(found_number_words[0]) in word_to_number.keys():
+            substring1 = word_to_number[found_number_words[0]]
+        else:
+            substring1=found_number_words[0]
+
+        if(found_number_words[-1]) in word_to_number.keys():
+            substring2 = word_to_number[found_number_words[-1]]
+        else:
+            substring2=found_number_words[-1]
+
+        strings=str(substring1) + str(substring2)
+        print(strings)
+        final_results=int(strings)+final_results
+
+
 print(final_results)
-
-
